4817a538280b7eaf8323a66e274f6a0ec9258673
Damon Kelley <damon.kelley@gmail.com>
2016-10-19 14:55:45 -0500

    Add the beginnings of the console application

--- src/TicTacToe/TicTacToe.fs
+++ src/TicTacToe/TicTacToe.fs
@@ -1,9 +1,51 @@
 module TicTacToe
 
-let add x y =
-    x + y
+open UI
+open Game
+open InputTransformer
+
+type TicTacToe =
+    { UI : UI
+    ; Game : Game
+    ; Presenter : Game -> string
+}
+
+let create ui presenter p1 p2 =
+    { UI = ui
+    ; Game = Game.create p1 p2
+    ; Presenter = presenter
+    }
+
+let presentOutcome = function
+    | {Game = {Outcome = Draw}} -> "Draw"
+    | {Game = {Outcome = Winner w}} -> sprintf "%s wins!" w
+    | _ -> ""
+
+let private transformer =
+    toWhitelistedInteger << availableSpaces
+
+let private userMove ttt =
+    ttt.UI.Prompt "-> " (transformer ttt.Game)
+
+let private present ttt =
+    presentOutcome ttt
+    |> ttt.UI.Write
+    |> ignore
+    ttt
+
+let private doTurn ttt =
+    {ttt with Game = play(ttt.Game, [userMove ttt])}
+    |> present
+
+let rec private loop ttt =
+    match doTurn ttt with
+    | {Game = {Outcome = InProgress}} as ttt -> loop ttt
+    | ttt -> ttt
+
+let rec start (ui:UI) game =
+    create ui (fun _ -> "") "X" "O"
+    |> loop
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
     0 // return an integer exit code
--- test/TicTacToe.Test/TicTacToe.Test.fs
+++ test/TicTacToe.Test/TicTacToe.Test.fs
@@ -2,12 +2,72 @@ module TicTacToe.Test
 
 open NUnit.Framework
 open FsUnit
+open TestHelpers
+
+open System.IO
+open System.Text
+
 open TicTacToe
 
-[<Test>]
-let ``Example Test`` () =
-    1 |> should equal 1
+#nowarn "0760"
+
+[<TearDown>]
+let ``reset stdin and stdout`` () =
+    resetIO()
 
 [<Test>]
-let ``import and test`` () =
-    add 1 2 |> should equal 3
+let ``create initializes a new TicTacToe record`` () =
+    let presenter game = ""
+    let console = Console.Console()
+
+    let expected =
+        { UI = console
+        ; Game = Game.create "X" "O"
+        ; Presenter = presenter
+        }
+
+    let actual = TicTacToe.create console presenter "X" "O"
+
+    expected.UI |> shouldEqual <| actual.UI
+    expected.Game |> shouldEqual <| actual.Game
+
+[<Test>]
+let ``the winner is shown on the UI`` () =
+    let output = new StringWriter() |> patchStdOut
+
+    System.String.Join("\n", [1; 4; 2; 5; 3;])
+    |> StringReader
+    |> patchStdIn
+    |> ignore
+
+    TicTacToe.start <| new Console.Console() <| Game.create "X" "O" |> ignore
+
+    output.ToString() |> should contain "X wins!"
+
+[<Test>]
+let ``Draw is shown if there is no winner`` () =
+    let output = new StringWriter() |> patchStdOut
+
+    System.String.Join("\n", [1; 3; 7; 4; 9; 5; 6; 8; 2])
+    |> StringReader
+    |> patchStdIn
+    |> ignore
+
+    TicTacToe.start <| new Console.Console() <| Game.create "X" "O" |> ignore
+
+    output.ToString() |> should contain "Draw"
+    output.ToString() |> should not' (contain "X wins!")
+
+[<Test>]
+let ``the board is presented after each move`` () =
+    let output = new StringWriter() |> patchStdOut
+
+    System.String.Join("\n", [1; 3; 7; 4; 9; 5; 6; 8; 2])
+    |> StringReader
+    |> patchStdIn
+    |> ignore
+
+    TicTacToe.start <| new Console.Console() <| Game.create "X" "O" |> ignore
+
+    output.ToString() |> should contain "Draw"
+    output.ToString() |> should not' (contain "X wins!")
